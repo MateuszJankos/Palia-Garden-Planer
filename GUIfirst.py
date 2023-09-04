aa8116e079c3e97d20f6165847b6bb4f086ec458
MateuszJankos <jankowskim1002@gmail.com>
2023-09-04 21:40:41 +0200

    Update 0.2v
    Added 9x9 soil grid

--- GUIfirst.py
+++ GUIfirst.py
@@ -1,11 +1,42 @@
 import tkinter as tk
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
+
+def on_button_click(row, col):
+    print(f"Kliknięto przycisk w rzędzie {row}, kolumnie {col}")
 
 root = tk.Tk()
 
-root.geometry("600x800") # Wymiary okna
-root.title("Palia Garden Planer Program") #Tytuł Programu na górze
+# Wymiary okna
+root.geometry("600x800")
 
-label = tk.Label(root, text="Set up your garden here", font=('Arial', 18)) #root jako tekst oraz personalizacja tekstu
-label.pack(padx=20, pady=20) #odstępy od krawędzi
+#Tytuł Programu na górze
+root.title("Palia Garden Planer Program")
 
+# Tworzenie ramki do umieszczenia siatki guzików
+frame = tk.Frame(root)
+frame.pack(padx=20, pady=20)
+
+# Wczytaj obraz i przekształć go na format obsługiwany przez Tkinter
+image = Image.open("soil.png")
+photo = ImageTk.PhotoImage(image)
+
+# Tworzenie siatki guzików z obrazem
+buttons = [[None for _ in range(9)] for _ in range(9)]
+
+for row in range(9):
+    for col in range(9):
+        button = tk.Button(frame, image=photo, width=50, height=50, command=lambda row=row, col=col: on_button_click(row, col))
+        button.grid(row=row, column=col, sticky="nsew") # Ustawiamy sticky na "nsew" dla każdego guzika
+
+# Konfigurujemy rozciąganie siatki guzików w ramce
+for i in range(9):
+    frame.columnconfigure(i, weight=1)
+    frame.rowconfigure(i, weight=1)
+
+# Dodajemy etykietę pod siatką guzików
+label = tk.Label(root, text="Set up your garden here")
+label.pack(pady=10)  # Dodajemy odstęp od etykiety do siatki guzików
+
+# Uruchomienie pętli głównej
 root.mainloop()
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Palia-Garden-Planer
  Garden planer for your crops in Palia
+
+# Used modules
+
+> tkinter - GUI module\
+> Pillow - for using images as buttons 
