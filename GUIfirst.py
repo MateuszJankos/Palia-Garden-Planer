6b8a20204fb58751a75eb42e6adb51e0c5b3640f
MateuszJankos <jankowskim1002@gmail.com>
2023-09-09 21:41:24 +0200

    Dodano Tło do okna obrazu

--- GUIfirst.py
+++ GUIfirst.py
@@ -24,14 +24,22 @@ def reset_garden():
     for row in range(9):
         for col in range(9):
             buttons[row][col].configure(image=photo)
-    
+
 root = tk.Tk()
-root.configure(bg='#303533')
-root.geometry("600x800")
+root.geometry("720x720")
 root.title("Palia Garden Planner Program")
 
+# Dodaj tło jako obrazek Tlo.png
+bg_image = Image.open("Tlo.png")
+bg_photo = ImageTk.PhotoImage(bg_image)
+bg_label = tk.Label(root, image=bg_photo)
+bg_label.place(relwidth=1, relheight=1)
+
+label = tk.Label(root, text="Garden")
+label.pack(pady=10)
+
 frame = tk.Frame(root)
-frame.pack(padx=20, pady=20)
+frame.pack(padx=20, pady=5)
 
 image = Image.open("soil.png")
 photo = ImageTk.PhotoImage(image)
@@ -39,27 +47,28 @@ photo = ImageTk.PhotoImage(image)
 buttons = [[None for _ in range(9)] for _ in range(9)]
 vegetable_images = {}
 
-selected_vegetable = None  # Zmienna do przechowywania wybranego warzywa
-garden_plan = [[None for _ in range(9)] for _ in range(9)]  # Tablica do przechowywania informacji o zasianych warzywach
+selected_vegetable = None
+garden_plan = [[None for _ in range(9)] for _ in range(9)]
 
 for row in range(9):
     for col in range(9):
         button = tk.Button(frame, image=photo, width=50, height=50, command=lambda row=row, col=col: on_button_click(row, col))
         button.grid(row=row, column=col, sticky="nsew")
-        buttons[row][col] = button  # Dodajemy guzik do tablicy guzików
+        buttons[row][col] = button
 
 label = tk.Label(root, text="Set up your garden here")
-label.pack(pady=10)
+label.pack(pady=5)
 
-# Tworzenie ramki dla 7 guzików poziomo
+label = tk.Label(root, text="Crops")
+label.pack(pady=5)
+
 button_frame = tk.Frame(root)
 button_frame.pack()
 
-# Dodajemy 7 guzików z obrazkami od 1 do 7
 for i in range(1, 8):
     digit_image = Image.open(f"{i}.png")
     digit_photo = ImageTk.PhotoImage(digit_image)
-    vegetable_images[i] = digit_photo  # Przechowujemy obrazki warzyw w słowniku
+    vegetable_images[i] = digit_photo
     button = tk.Button(button_frame, image=digit_photo, width=50, height=50, command=lambda i=i: select_vegetable(i))
     button.pack(side=tk.LEFT, padx=5)
 
